ccbb15d6398b0eca34fdeaf0cabf5f957091931a
Ashutosh <ashutosh.developer01@gmail.com>
2022-07-14 15:29:16 +0530

    add new sol

---
+++ Arrays/Arrays_Question19.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Arrays_Question19{
+	public static void main(String[] args) {
+	
+	int arr[] = {12,35,1,10,23,34,1};
+	int n = arr.length;
+
+	findSecondlargest(arr, n);	
+	
+	}
+
+	public static void findSecondlargest(int arr[], int n){
+
+		int i, first, second;
+ 
+ 
+    int largest = second = Integer.MIN_VALUE;
+ 
+    // Find the largest element
+    for(i = 0; i < n; i++)
+    {
+        largest = Math.max(largest, arr[i]);
+    }
+ 
+    // Find the second largest element
+    for(i = 0; i < n; i++)
+    {
+        if (arr[i] != largest)
+            second = Math.max(second, arr[i]);
+    }
+    if (second == Integer.MIN_VALUE)
+        System.out.printf("There is no second " +
+                          "largest element\n");
+    else
+        System.out.printf("The second largest " +
+                          "element is %d\n", second);}
+
+}
