10400d9f4cb2e14df2921ac0d4c3f65c3c49f2d4
Ashutosh <ashutosh.developer01@gmail.com>
2022-08-04 19:55:57 +0530

    new patterns

--- Pattern/Pattern10.java
+++ Pattern/Pattern10.java
@@ -3,18 +3,40 @@ public class Pattern10 {
 		
 		int n = 4;
 
+		//upper part
 		for(int i = 1; i<=n; i++){
 
 			for(int j = 1; j<=i; j++){
-				System.out.print("*\t");
+				System.out.print("*");
+			}
+
+			int spaces = 2*(n-i);
+			for(int j = 1; j<=spaces; j++){
+				System.out.print(" ");
 			}
 			
+			for(int j = 1; j<= i; j++){
+				System.out.print("*");
+			}
 			System.out.println();
+		}
 
-			for(int j = n-1; j>=0; j--){
-				System.out.println("\t");
+		// lower part
+		for(int i = n; i>=0; i--){
+
+			for(int j = 1; j<=i; j++){
+				System.out.print("*");
 			}
 
+			int spaces = 2*(n-i);
+			for(int j = 1; j<=spaces; j++){
+				System.out.print(" ");
+			}
+
+			for(int j = 1; j<= i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 	
