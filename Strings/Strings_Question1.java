2d4010558efee0b3ce27da21fc2c862cdf9fd887
Ashutosh <ashutosh.developer01@gmail.com>
2022-09-08 19:49:43 +0530

    new method to reverse string by string builder

--- Strings/Strings_Question1.java
+++ Strings/Strings_Question1.java
@@ -1,19 +1,43 @@
-// Java program to Reverse a String by
-// converting string to characters one
-// by one
-/*import java.lang.*;
-import java.io.*;
-import java.util.*;
-*/
+/*Java program to Reverse a String by
+converting string to characters one
+by one */
 
 // Class of ReverseString
-class String_Question1 {
-	public static void main(String[] args)
-	{
-		String input = "GeeksForGeeks";
+
+class String_Question1{
+	public static void main(String[] args){
+
+		String input1 = "GeeksForGeeks";
 
 		//reverseString1(input);
-		reverseString2(input);
+		//reverseString2(input);
+		reverseString3(input1);
+	}
+
+
+	public static void reverseString3(String input1){
+		char[] cArray = input1.toCharArray();
+		StringBuilder tempS = new StringBuilder();
+		String result = "";
+
+		for(int i = 0; i < cArray.length; i++){
+			if(cArray[i] != ' '){
+				if(i == cArray.length-1){
+					tempS.append(String.valueOf(cArray[i]).toUpperCase());
+				}
+				else if(cArray[i+1] == ' '){
+					tempS.append(String.valueOf(cArray[i]).toUpperCase());
+				}else{
+					tempS.append(String.valueOf(cArray[i]).toUpperCase());
+				}
+			}
+			else{
+				tempS.reverse();
+				result += tempS;
+				tempS.append("");
+			}
+		}
+		System.out.println(tempS);
 	}
 
 	public static void reverseString2(String input){
