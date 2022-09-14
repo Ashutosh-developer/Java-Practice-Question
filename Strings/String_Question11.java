d3e566e8efd58f92aa4971d5269ce5e7ce8124a5
Ashutosh <ashutosh.developer01@gmail.com>
2022-09-14 23:16:46 +0530

    new String sol longestCommonPrefix

--- Strings/String_Question11.java
+++ Strings/String_Question11.java
@@ -15,7 +15,7 @@ public class String_Question11{
 
 		for(int i = 1; i < strs.length; i++){
 			while(strs[i].indexOf(prefix) != 0){
-				prefix = prefix.substring(0,prefix.length-1);
+				prefix = prefix.substring(0,prefix.length()-1);
 				if(prefix.isEmpty())
 					return"";
 			}
