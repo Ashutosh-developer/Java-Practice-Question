9b34091ba025480b301bd99c4dd397df5e97d77e
Ashutosh <ashutosh.developer01@gmail.com>
2022-09-16 21:32:26 +0530

    String Questin

---
+++ Strings/String_Question13.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class String_Question13{
+	public static void main(String[] args) {
+		String s = "abaacdaefaah";
+		char search = 'n';
+
+		/*if(firstOccurance(s,search) == -1){
+			System.out.println("The character is not present in String");
+		}else{
+			System.out.println(firstOccurance(s,search));
+		System.out.println(lastOccurance(s,search));
+		}*/
+
+		getIndices(s,'a',0);
+
+		System.out.println("First occurence : " + first);
+		System.out.println("Last occurence : " + last);
+	}
+
+	public static int first = -1;
+	public static int last = -1;
+
+	public static void getIndices(String str, char el, int idx) {
+		if(idx == str.length()) {
+			// System.out.println(first);
+			// System.out.println(last);
+			return;
+		}
+		if(str.charAt(idx) == el) {
+			if(first == -1) {
+				first = idx;
+			} else {
+			last = idx;
+		}
+	}
+	getIndices(str, el, idx+1);
+}
+
+	public static int firstOccurance(String s, char search){
+		int temp = -1;
+		for(int i = 0; i < s.length(); i++){
+			if(search == s.charAt(i)){
+				temp = i;
+				break;
+			}else{
+				//System.out.println("String not found");
+				temp = -1;
+			}
+		}
+		return temp;
+	}
+
+	public static int lastOccurance(String s, char search){
+		int temp = -1;
+		for(int i = 0; i < s.length(); i++){
+			if(search == s.charAt(i)){
+				temp = i;
+			}
+		}
+		return temp;
+	}
+}
---
+++ Strings/String_Question14.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class String_Question14{
+	public static void main(String args[]){
+		String str = "3[b2[ca]]";
+        System.out.println(decodeString(str));
+	}
+
+	public static String decodeString(String s){
+		Stack<Integer> numStack = new Stack <>();
+		Stack<String> strStack = new Stack <>();
+		StringBuilder sb = new StringBuilder();
+
+		int len  = s.length();
+		for(int i = 0; i < len; i++){
+			char ch = s.charAt(i);
+
+			if(Character.isDigit(ch)){
+				int num = ch - '0';
+				while(i + 1 < len && Character.isDigit(s.charAt(i+1))){
+					num = num * 10 + s.charAt(i+1) - '0';
+					i++;
+				}
+				numStack.push(num);
+			}else if(ch == '['){
+				strStack.push(sb.toString());
+				sb = new StringBuilder();
+			}else if(ch == ']'){
+				int k = numStack.pop();
+				StringBuilder tmp = new StringBuilder(strStack.pop());
+
+				for(int j = 0; j < k; j++){
+					tmp.append(sb);
+				}
+				sb = tmp;
+			}else{
+				sb.append(ch);
+			}
+		}
+		return sb.toString();
+	}
+}
