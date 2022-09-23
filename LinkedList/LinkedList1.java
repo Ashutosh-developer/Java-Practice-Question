4d31bc4e683401ba19c50481d05224461fef2626
Ashutosh <ashutosh.developer01@gmail.com>
2022-09-23 21:00:19 +0530

    Linked list search

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -6,40 +6,19 @@ import java.util.LinkedList;
 
 class LinkedList1 {
 	public static void main(String[] args) {
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
+		LinkedList list = new LinkedList();
 
-		LinkedList1 l = new LinkedList1();
-		l.disp(list);
+		list.addFirst(1);
+		list.addFirst(5);
+		list.addFirst(7);
+		list.addFirst(3);
+		list.addFirst(8);
+		list.addFirst(2);
+		list.addFirst(3);
 
-		list.removeFirst();
 
+		System.out.println(list);
 
-
-	
-	}
-
-	public void reverseList(LinkedList list){
-		Node curr = this.head;
-		Node prev = null;
-		Node next = null;
-
-		
-	}
-
-	public void disp(LinkedList list){
-			// System.out.println(list.toArray());	
-		// System.out.println("LinkedList: "+ list);
-         
-        //Convert LinkedList into an Array the method has no parameter
-        Object[] a = list.toArray();
-       
-        //Print all elements of the Array
-        // System.out.print("After converted LinkedList to Array: ");
-        for(Object element : a)
-            System.out.println(element+" ");
-		}
-		
+		System.out.println(list.indexOf(7));
+	} 
 }
