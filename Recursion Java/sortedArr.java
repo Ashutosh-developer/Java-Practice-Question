65bb0917d4e6d86ba213307385564a653c2a6c18
Ashutosh <ashutosh.developer01@gmail.com>
2022-09-30 14:00:43 +0530

    is array sorted through recursion

---
+++ Recursion Java/sortedArr.java
@@ -0,0 +1,19 @@
+public class sortedArr{
+	public static void main(String[] args) {
+		int arr [] = {1,3,6,7,8};
+
+		System.out.println(isSorted(arr,0));
+	}
+
+	public static boolean isSorted(int arr[], int idx){
+		if(idx == arr.length-1){
+			return true;
+		}
+
+		if(arr[idx] < arr[idx+1]){
+			return isSorted(arr, idx+1);
+		}else{
+			return false;
+		}
+	}
+}
