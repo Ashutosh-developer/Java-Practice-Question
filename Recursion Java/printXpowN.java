ddb10bc18423b014ccd51fc00b3c6516b3eac872
Ashutosh <ashutosh.developer01@gmail.com>
2022-10-24 20:43:34 +0530

    print x power n with stack height logn

--- Recursion Java/printXpowN.java
+++ Recursion Java/printXpowN.java
@@ -1,10 +1,28 @@
 class printXpowN{
 	public static void main(String[] args) {
 		int x = 2, n = 4;
-		int ans = calcPower(x,n);
+		int ans = calcPower1(x,n);
 		System.out.println(ans);
 	}
 
+
+	public static int calcPower1(int x, int n){
+		if(n==0){
+			return 1;
+		}
+
+		if(x == 0){
+			return 0;
+		}
+
+		//if n is even
+		if(n % 2 == 0){
+			return calcPower1(x,n/2) * calcPower1(x,n/2);
+		}else{
+			//n is odd
+			return calcPower1(x,n/2) * calcPower1(x,n/2) * x;
+		}
+	}
 	public static int calcPower(int x, int n){
 		if(n == 0){
 			return 1;
