61a4dc985a575b4c9c607f6aad05fd72cb75c55b
Ashutosh <ashutosh.developer01@gmail.com>
2022-10-28 21:38:15 +0530

    new recursion solution keypad permutation

---
+++ Recursion Java/printKeyCom.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+public class printKeyCom{
+	public static void main(String[] args) {
+		String str = "23";
+		printComb(str, 0, " ");
+	}
+
+	public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+	public static void printComb(String str, int idx, String combination){
+
+		if(idx == str.length()){
+			System.out.println(combination);
+			return;
+		}
+		char currChar = str.charAt(idx);
+		String mapping = keypad[currChar -'0'];
+
+		for(int i = 0; i < mapping.length(); i++){
+			printComb(str, idx+1, combination+mapping.charAt(i));
+ 		}
+ 	}
+ }
