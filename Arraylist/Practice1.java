a77ea7128d95cc25da121176ebdc8aa464f554e0
Ashutosh <ashutosh.developer01@gmail.com>
2023-04-25 21:03:53 +0530

    Arraylist practice

---
+++ Arraylist/Practice1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Practice1{
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		ArrayList<String> list2 = new ArrayList<String>();
+		ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+
+		list.add(10);
+		list.add(11);
+		list.add(12);
+		list.add(13);
+
+		System.out.println(list);
+	}
+}
